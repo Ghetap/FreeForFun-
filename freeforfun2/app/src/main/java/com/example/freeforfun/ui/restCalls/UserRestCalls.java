f71fa3df522217f922f89cb963b5aec0dc8c5ace
Sonya <sonyaparau@yahoo.com>
2019-11-15 22:12:52 +0200

    Login and Logout functionalities

--- freeforfun2/app/src/main/java/com/example/freeforfun/ui/restCalls/UserRestCalls.java
+++ freeforfun2/app/src/main/java/com/example/freeforfun/ui/restCalls/UserRestCalls.java
@@ -1,5 +1,7 @@
 package com.example.freeforfun.ui.restCalls;
 
+import android.os.StrictMode;
+
 import com.example.freeforfun.ui.utils.HttpUtils;
 import com.example.freeforfun.ui.utils.Paths;
 import com.loopj.android.http.JsonHttpResponseHandler;
@@ -10,8 +12,11 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
 import java.net.HttpURLConnection;
 import java.net.URL;
 
@@ -19,25 +24,38 @@ public class UserRestCalls {
 
     private static final String BASE_URL = Paths.BASE_URL;
 
-    public static String login(String username, String password) throws IOException {
-//        String url = BASE_URL + Paths.LOGIN + "/" + username + "/" + password;
-//        URL urlForGetRequest = new URL(url);
-//        String readLine;
-//        HttpURLConnection connection = (HttpURLConnection) urlForGetRequest.openConnection();
-//        connection.setRequestMethod("POST");
-//        //conection.setRequestProperty("userId", "a1bcdef"); // set userId its a sample here
-//        int responseCode = connection.getResponseCode();
-//        if (responseCode == HttpURLConnection.HTTP_OK) {
-//            BufferedReader in = new BufferedReader(
-//                    new InputStreamReader(connection.getInputStream()));
-//            StringBuffer response = new StringBuffer();
-//            while ((readLine = in .readLine()) != null) {
-//                response.append(readLine);
-//            } in .close();
-//           return "JSON String Result " + response.toString();
-//            //GetAndPost.POSTRequest(response.toString());
-//        } else {
-           return "POST NOT WORKED";
-//        }
+    public static String login(String username, String password){
+        String url = BASE_URL + Paths.LOGIN + "/" + username + "/" + password;
+
+        if (android.os.Build.VERSION.SDK_INT > 9)
+        {
+            StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+            StrictMode.setThreadPolicy(policy);
+        }
+        try {
+            URL urlForGetRequest = new URL(url);
+            String readLine;
+            HttpURLConnection connection = (HttpURLConnection) urlForGetRequest.openConnection();
+            connection.setReadTimeout(15000);
+            connection.setConnectTimeout(15000);
+            connection.setRequestMethod("POST");
+            connection.setDoInput(true);
+            connection.setDoOutput(true);
+            connection.connect();
+            int responseCode = connection.getResponseCode();
+            if (responseCode == HttpURLConnection.HTTP_OK){
+                BufferedReader in = new BufferedReader(
+                        new InputStreamReader(connection.getInputStream()));
+                StringBuilder response = new StringBuilder();
+                while ((readLine = in .readLine()) != null) {
+                    response.append(readLine);
+                } in .close();
+                return response.toString();
+            }
+        } catch(IOException ex ){
+            ex.printStackTrace();
+        }
+        return null;
+
     }
 }

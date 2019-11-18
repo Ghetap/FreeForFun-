813ccabdab16f76361642d49110fc5ab1717c707
Sonya <sonyaparau@yahoo.com>
2019-11-18 22:04:44 +0200

    Merge branch 'develop' of https://github.com/Ghetap/FreeForFun- into feature_sonya
    # Conflicts:
    #	freeforfun2/app/src/main/java/com/example/freeforfun/ui/restCalls/UserRestCalls.java

--- freeforfun2/app/src/main/java/com/example/freeforfun/ui/login/RegisterActivity.java
+++ freeforfun2/app/src/main/java/com/example/freeforfun/ui/login/RegisterActivity.java
@@ -7,9 +7,12 @@ import android.content.Context;
 import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.text.method.HideReturnsTransformationMethod;
+import android.text.method.PasswordTransformationMethod;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.android.volley.RequestQueue;
@@ -120,6 +123,19 @@ public class RegisterActivity extends AppCompatActivity {
                 .make(coordinatorLayout, messageFromServer, Snackbar.LENGTH_LONG)
                 .setActionTextColor(Color.MAGENTA);
         snackbar.show();
+    }
+
+    public void showHidePassword(View view){
+        if(view.getId()==R.id.show_password_btn){
+            if(password.getTransformationMethod().equals(PasswordTransformationMethod.getInstance())){
+                ((ImageView)(view)).setImageResource(R.drawable.hidepassword);
+                password.setTransformationMethod(HideReturnsTransformationMethod.getInstance());
+            }
+            else{
+                ((ImageView)(view)).setImageResource(R.drawable.showpassword);
+                password.setTransformationMethod(PasswordTransformationMethod.getInstance());
+            }
+        }
     }
     public boolean validateUsername(String usernameInput){
         if(!UserValidations.isNotEmpty(usernameInput)) {

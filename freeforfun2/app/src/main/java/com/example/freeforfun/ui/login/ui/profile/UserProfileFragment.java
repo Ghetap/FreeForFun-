923309de09d112917bf87629c34a25e29fb48ccb
Parau, Sonya (EXTERN; msg DAVID) <sonya.parau@msg.group>
2019-12-28 22:29:03 +0200

    View details of a local.

--- freeforfun2/app/src/main/java/com/example/freeforfun/ui/login/ui/profile/UserProfileFragment.java
+++ freeforfun2/app/src/main/java/com/example/freeforfun/ui/login/ui/profile/UserProfileFragment.java
@@ -5,13 +5,12 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
-
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProviders;
-
 import com.example.freeforfun.R;
 import com.example.freeforfun.ui.login.MainActivity;
+
 public class UserProfileFragment extends Fragment {
 
     private UserProfileViewModel userProfileViewModel;
